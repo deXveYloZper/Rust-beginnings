96c7fbc2e29c64b78f1acc1febede03f6306fc9e
deXveYloZper <xyzdeveloper@outlook.com>
2025-01-10 18:59:19 +0100

    ok let's get started

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+
+    println!("Hello, World!");
+
+    let name = "Kristian";
+
+    println!("Hello, {}", name);
+     let mut board = [' '; 9];
+
+    board[0] = 'X';
+    board[4] = 'X';
+    board[8] = 'X';
+
+    println!(
+        "
+
+    +---+---+---+
+    | {} | {} | {} |
+    +---+---+---+
+    | {} | {} | {} |
+    +---+---+---+
+    | {} | {} | {} |
+    +---+---+---+
+    ",
+            board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]
+    );
+
 }
