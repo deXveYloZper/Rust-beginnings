85f88844dda350eefb2bc39b38d6c728a358d70d
deXveYloZper <xyzdeveloper@outlook.com>
2025-01-11 20:38:57 +0100

    clean up and moving to structs and enums reading

--- src/main.rs
+++ src/main.rs
@@ -2,39 +2,19 @@ use std::io;
 
 fn main() {
 
-    println!("Hello, World!");
-
-    let name = "Kristian";
-
-    println!("Hello, {}", name);
-     let mut board = [' '; 9];
+    let mut board = [' '; 9];
 
     board[0] = 'X';
     board[4] = 'X';
     board[8] = 'X';
 
-    println!(
-        "
-
-    +---+---+---+
-    | {} | {} | {} |
-    +---+---+---+
-    | {} | {} | {} |
-    +---+---+---+
-    | {} | {} | {} |
-    +---+---+---+
-    ",
-            board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]
-    );
-
-    let mut input = String::new();
-
-    io::stdin().read_line(&mut input).expect("Error reading input");
-
-    println!("{input}")
-
-
-
+    print_board(board);
+    loop {
+        let input = get_input();
+        if input.trim() == "quit" {break;}
+        println!("{input}")
+    }
+    
 }
 
 fn print_board(board: [char;9]){
@@ -49,6 +29,15 @@ fn print_board(board: [char;9]){
         | {} | {} | {} |
         +---+---+---+
         ",
-                board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]
+        board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]
     )
+}
+
+fn get_input() -> String{
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Error whilst reading the input");
+    
+
+    input
 }
