ed4f617a63d52fd1e02c9075461d4d93b7b3d53f
deXveYloZper <xyzdeveloper@outlook.com>
2025-01-11 19:16:54 +0100

    borrowing and ownership, functions, vectors and arrays

--- src/learning.rs
+++ src/learning.rs
@@ -6,3 +6,11 @@ let slice2 = &string[0..5];
 
 println!(slice2, slice);
 }
+
+fn borrowing_and_ownesrship(){
+    let a = vec![1,2];
+    let b = a;
+
+    println!("{a:?}");
+    println!(a:?);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn main() {
 
     println!("Hello, World!");
@@ -25,4 +27,28 @@ fn main() {
             board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]
     );
 
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).expect("Error reading input");
+
+    println!("{input}")
+
+
+
+}
+
+fn print_board(board: [char;9]){
+    println!(
+        "
+
+        +---+---+---+
+        | {} | {} | {} |
+        +---+---+---+
+        | {} | {} | {} |
+        +---+---+---+
+        | {} | {} | {} |
+        +---+---+---+
+        ",
+                board[0], board[1], board[2], board[3], board[4], board[5], board[6], board[7], board[8]
+    )
 }
