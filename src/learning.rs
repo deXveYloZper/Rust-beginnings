2e88bc8c37ddddc01a2c2b652be07a8d56b37a78
deXveYloZper <xyzdeveloper@outlook.com>
2025-01-12 20:41:17 +0100

    structs

--- src/learning.rs
+++ src/learning.rs
@@ -14,3 +14,31 @@ fn borrowing_and_ownesrship(){
     println!("{a:?}");
     println!(a:?);
 }
+
+//Structs
+
+struct Person{
+    name: String,
+    age: u32,
+}
+
+
+impl Person{
+    fn new(name: String, age: u32)-> Self{
+        Person {name, age}
+    }
+    fn say_name(&self){
+        println!("Hi, my name is {}", self.name)
+    }
+
+    fn change_age(&mut self, age:u32){
+        self.age = age;
+    }
+}
+
+fn main(){
+    let mut bob = Person::new("bob".to_string(), 30);
+
+    bob.say_name();
+    bob.change_age(28)
+}
